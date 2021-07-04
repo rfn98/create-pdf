fed14dae56d0fe8f803f1a2081496857cac707c7
rifanajie <rifanajieagung48@gmail.com>
2021-07-04 16:21:58 +0700

    pdf surat keterangan domisili

---
+++ content-pdf.js
@@ -0,0 +1,8 @@
+const content = [
+	{ text:'PEMERINTAH KABUPATEN INDRAMAYU', style: { alignment: 'center', fontSize: 17, bold: true }, margin: [0, 0, 0, 8] },
+	{ text:'KECAMATAN LOSARANG', style: { alignment: 'center', fontSize: 17, bold: true }, margin: [0, 0, 0, 8] },
+	{ text:'DESA RANJENG', style: { alignment: 'center', fontSize: 17, characterSpacing: 5, bold: true }, margin: [0, 0, 0, 8] },
+	{ canvas: [ { type: 'line', x1: 0, y1: 0, x2: 515, y2: 0, lineWidth: 3 } ], margin: [0, 0, 0, 25]},
+	
+	// { image: 'desa' }
+]
